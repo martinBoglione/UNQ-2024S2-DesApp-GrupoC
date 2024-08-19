a1b0a401f6ffe5b6c2149715c6670a25203b3915
Martin <martinboglione97@gmail.com>
2024-08-18 21:12:41 -0300

    Project set up

---
+++ src/test/java/ar/edu/unq/DevApp/DevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.DevApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
