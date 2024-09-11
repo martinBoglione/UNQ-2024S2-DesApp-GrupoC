1f27970e376f09314d9d2a8ae7f46c4393c0e28b
Lea MM <leatex_99@yahoo.com>
2024-09-11 03:48:59 -0300

    configuring swagger

--- src/main/java/ar/edu/unq/DevApp/webservice/CryptoController.java
+++ src/main/java/ar/edu/unq/DevApp/webservice/CryptoController.java
@@ -1,12 +1,15 @@
 package ar.edu.unq.DevApp.webservice;
 
 import ar.edu.unq.DevApp.service.CryptoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("")
+@Tag(name = "Cryptos", description = "Cryptos APIs")
 public class CryptoController {
 
     @Autowired
