1f1db5b011e0509efb964d8c5ad3f0461bf2abba
Lea MM <leatex_99@yahoo.com>
2024-09-11 18:34:54 -0300

    swagger working

--- src/main/java/ar/edu/unq/DevApp/webservice/CryptoController.java
+++ src/main/java/ar/edu/unq/DevApp/webservice/CryptoController.java
@@ -3,15 +3,23 @@ package ar.edu.unq.DevApp.webservice;
 import ar.edu.unq.DevApp.service.CryptoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@RequestMapping("")
 @Tag(name = "Cryptos", description = "Cryptos APIs")
+@RestController
+@RequestMapping("/api")
 public class CryptoController {
 
     @Autowired
     CryptoService cryptoService;
+
+    @GetMapping("/users")
+    public ResponseEntity<HttpStatus> getAllUsers() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
