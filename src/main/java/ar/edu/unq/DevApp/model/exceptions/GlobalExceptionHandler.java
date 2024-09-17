172a0016f44de1283ae659c4405e365742aebd22
Martin <martinboglione97@gmail.com>
2024-09-16 21:32:18 -0300

    Sonarcloud fixes

--- src/main/java/ar/edu/unq/DevApp/model/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ar/edu/unq/DevApp/model/exceptions/GlobalExceptionHandler.java
@@ -10,37 +10,37 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidNameException.class)
-    public ResponseEntity<?> handleInvalidNameException(InvalidNameException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidNameException(InvalidNameException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidEmailException.class)
-    public ResponseEntity<?> handleInvalidEmailException(InvalidEmailException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(InvalidEmailException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidAddressException.class)
-    public ResponseEntity<?> handleInvalidEmailException(InvalidAddressException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(InvalidAddressException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<?> handleInvalidEmailException(InvalidPasswordException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(InvalidPasswordException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidCVUException.class)
-    public ResponseEntity<?> handleInvalidEmailException(InvalidCVUException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(InvalidCVUException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidWalletException.class)
-    public ResponseEntity<?> handleInvalidEmailException(InvalidWalletException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(InvalidWalletException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> handleInvalidEmailException(UserNotFoundException ex, WebRequest request) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleInvalidEmailException(UserNotFoundException ex, WebRequest request) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/ar/edu/unq/DevApp/utils/WebSecurityConfig.java
+++ src/main/java/ar/edu/unq/DevApp/utils/WebSecurityConfig.java
@@ -2,31 +2,13 @@ package ar.edu.unq.DevApp.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http.authorizeHttpRequests()
-//                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-//                .and()
-//                .csrf().ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
-//                .and()
-//                .headers(headers -> headers.frameOptions().sameOrigin())
-//                .build();
-//    }
-
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().anyRequest();
+        return web -> web.ignoring().anyRequest();
     }
 }
