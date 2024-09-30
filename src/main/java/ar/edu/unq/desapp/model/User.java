91584a11281a1e0c211e5cbd5f9b69b397f81471
leatex <leatex_99@yahoo.com>
2024-09-29 21:41:43 -0300

    #4 quotes #6 magic numbers

--- src/main/java/ar/edu/unq/desapp/model/User.java
+++ src/main/java/ar/edu/unq/desapp/model/User.java
@@ -1,20 +1,26 @@
 package ar.edu.unq.desapp.model;
 
 import ar.edu.unq.desapp.model.exceptions.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import lombok.*;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    //@Column(nullable = false, unique=true)
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(nullable = false)
     @Size(min=3, max=30)
@@ -34,6 +40,7 @@ public class User {
 
     @Column(nullable = false)
     @Size(min=6)
+    @JsonIgnore
     //@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).*$", message = "La contraseña debe tener al menos una minúscula, una mayúscula, un carácter especial y tener una longitud mínima de 6 caracteres.")
     private String password;
 
@@ -44,18 +51,6 @@ public class User {
     @Column(nullable = false)
     @Size(min=8, max=8)
     private String walletAddress;
-
-    public User(String name, String surname, String email, String address, String password, String cvu, String walletAddress) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.address = address;
-        this.password = password;
-        this.cvu = cvu;
-        this.walletAddress = walletAddress;
-    }
-
-    public User() {}
 
     public void validateUser() {
         if(!isValidName(this.name,this.surname))  {throw new InvalidNameException();}
@@ -99,63 +94,5 @@ public class User {
 
     public boolean isValidWallet(String walletAddress) {
         return walletAddress.length() == 8;
-    }
-
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCvu() {
-        return cvu;
-    }
-
-    public void setCvu(String cvu) {
-        this.cvu = cvu;
-    }
-
-    public String getWalletAddress() {
-        return walletAddress;
-    }
-
-    public void setWalletAddress(String walletAddress) {
-        this.walletAddress = walletAddress;
     }
 }
