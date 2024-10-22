5075cf98da5aca559c4d04aefe621c3495c371af
Martin <martinboglione97@gmail.com>
2024-10-22 20:29:19 -0300

    Fix exception text

--- src/main/java/ar/edu/unq/desapp/model/exceptions/InvalidWalletException.java
+++ src/main/java/ar/edu/unq/desapp/model/exceptions/InvalidWalletException.java
@@ -3,6 +3,6 @@ package ar.edu.unq.desapp.model.exceptions;
 public class InvalidWalletException extends RuntimeException {
 
     public InvalidWalletException() {
-        super("The CVU length must be 8 characters");
+        super("The Wallet length must be 8 characters");
     }
 }
