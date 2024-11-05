dbc666b4c5cd6f782a446b0ad9ff8ce2d3a944ed
Martin <martinboglione97@gmail.com>
2024-11-04 22:08:54 -0300

    User unit tests

---
+++ src/test/java/ar/edu/unq/desapp/UserTest.java
@@ -0,0 +1,121 @@
+package ar.edu.unq.desapp;
+
+import ar.edu.unq.desapp.model.User;
+import ar.edu.unq.desapp.model.exceptions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = User.builder()
+                .name("Martin")
+                .surname("Boglione")
+                .email("martinboglione97@gmail.com")
+                .address("Segurola y Habana 4310")
+                .password("Maradona10!")
+                .cvu("1234567890123456789012")
+                .walletAddress("12345678")
+                .build();
+    }
+
+    @Test
+    public void testValidUser() {
+        Assertions.assertDoesNotThrow(() -> user.validateUser());
+    }
+
+    @Test
+    public void testInvalidNameThrowsException() {
+        user.setName("Ma");
+        Exception exception = Assertions.assertThrows(InvalidNameException.class, user::validateUser);
+        Assertions.assertEquals(InvalidNameException.class, exception.getClass());
+
+        user.setName("Martin");
+        user.setSurname("B");
+        exception = Assertions.assertThrows(InvalidNameException.class, user::validateUser);
+        Assertions.assertEquals(InvalidNameException.class, exception.getClass());
+    }
+
+    @Test
+    public void testInvalidEmailThrowsException() {
+        user.setEmail("invalid-email");
+        Exception exception = Assertions.assertThrows(InvalidEmailException.class, user::validateUser);
+        Assertions.assertEquals(InvalidEmailException.class, exception.getClass());
+    }
+
+    @Test
+    public void testInvalidAddressThrowsException() {
+        user.setAddress("Short");
+        Exception exception = Assertions.assertThrows(InvalidAddressException.class, user::validateUser);
+        Assertions.assertEquals(InvalidAddressException.class, exception.getClass());
+    }
+
+    @Test
+    public void testInvalidPasswordThrowsException() {
+        user.setPassword("weak"); // No uppercase or special characters
+        Exception exception = Assertions.assertThrows(InvalidPasswordException.class, user::validateUser);
+        Assertions.assertEquals(InvalidPasswordException.class, exception.getClass());
+
+        user.setPassword("StrongPass"); // Missing special character
+        exception = Assertions.assertThrows(InvalidPasswordException.class, user::validateUser);
+        Assertions.assertEquals(InvalidPasswordException.class, exception.getClass());
+    }
+
+    @Test
+    public void testInvalidCVUThrowsException() {
+        user.setCvu("12345678901234567890"); // Less than 22 characters
+        Exception exception = Assertions.assertThrows(InvalidCVUException.class, user::validateUser);
+        Assertions.assertEquals(InvalidCVUException.class, exception.getClass());
+    }
+
+    @Test
+    public void testInvalidWalletAddressThrowsException() {
+        user.setWalletAddress("1234567"); // Less than 8 characters
+        Exception exception = Assertions.assertThrows(InvalidWalletException.class, user::validateUser);
+        Assertions.assertEquals(InvalidWalletException.class, exception.getClass());
+    }
+
+    @Test
+    public void testIsValidName() {
+        Assertions.assertEquals(true, user.isValidName("Martin", "Boglione"));
+        Assertions.assertEquals(false, user.isValidName("Ma", "Boglione"));
+        Assertions.assertEquals(false, user.isValidName("Martin", "B"));
+    }
+
+    @Test
+    public void testIsValidEmail() {
+        Assertions.assertEquals(true, user.isValidEmail("martinboglione97@gmail.com"));
+        Assertions.assertEquals(false, user.isValidEmail("invalid-email"));
+    }
+
+    @Test
+    public void testIsValidAddress() {
+        Assertions.assertEquals(true, user.isValidAddress("Segurola y Habana 4310"));
+        Assertions.assertEquals(false, user.isValidAddress("Short"));
+    }
+
+    @Test
+    public void testIsValidPassword() {
+        Assertions.assertEquals(true, user.isValidPassword("Maradona10!"));
+        Assertions.assertEquals(false, user.isValidPassword("weak"));
+        Assertions.assertEquals(false, user.isValidPassword("StrongPass"));
+    }
+
+    @Test
+    public void testIsValidCVU() {
+        Assertions.assertEquals(true, user.isValidCVU("1234567890123456789012"));
+        Assertions.assertEquals(false, user.isValidCVU("12345678901234567890"));
+    }
+
+    @Test
+    public void testIsValidWalletAddress() {
+        Assertions.assertEquals(true, user.isValidWallet("12345678"));
+        Assertions.assertEquals(false, user.isValidWallet("1234567"));
+    }
+}
