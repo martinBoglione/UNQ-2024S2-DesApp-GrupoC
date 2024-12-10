391b27f17a70656299cd04d4361fb759aa551e8b
Lea MM <leatex_99@yahoo.com>
2024-12-10 19:09:08 -0300

    Last quality fixes

--- src/test/java/ar/edu/unq/desapp/webservice/QuotesControllerTest.java
+++ src/test/java/ar/edu/unq/desapp/webservice/QuotesControllerTest.java
@@ -1,9 +1,7 @@
 package ar.edu.unq.desapp.webservice;
 
 import ar.edu.unq.desapp.model.Crypto;
-import ar.edu.unq.desapp.model.User;
 import ar.edu.unq.desapp.service.BinanceCryptoService;
-import ar.edu.unq.desapp.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,14 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
